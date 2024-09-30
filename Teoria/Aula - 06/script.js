1325f2b5f5e7804872c98b9f1c37c57d9e8b5b02
Henrique Simões <henrique_barros_simoes@hotmail.com>
2024-09-30 20:06:03 -0300

    Aula - 06

---
+++ Teoria/Aula - 06/script.js
@@ -0,0 +1,30 @@
+//let nome = prompt("insira seu nome");
+//let idade = parseInt(prompt("insira a sua idade"));
+//let ano_atual = 2024;
+
+//let ano_nascimento = ano_atual - idade;
+
+//let resposta = "olá, " + nome + ". O seu ano de nascimento é: " + ano_nascimento;
+
+//document.getElementById("resp").innerHTML = resposta;
+
+function soma(a, b){
+    let c = a + b;
+    return c;
+}
+
+let x = parseInt(prompt("insira o primeiro valor"));
+let y = parseInt(prompt("insira o segundo valor"));
+document.getElementById("resp").innerHTML = soma(x, y);
+
+function mult(a, b){
+    let c = a * b;
+    return c;
+}
+
+function ex2(){
+    numero = document.getElementById("i1").value;
+    document.getElementById("r2").innerHTML = mult(numero, 3);
+
+}
+

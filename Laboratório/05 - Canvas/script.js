2368cfdef80c52349788ec7109cc0b4d05ccf88c
Henrique Simões <henrique_barros_simoes@hotmail.com>
2024-10-07 22:19:56 -0300

    Canvas

--- Laboratório/05 - Canvas/script.js
+++ Laboratório/05 - Canvas/script.js
@@ -1,99 +1,102 @@
 let canvas=document.getElementById("canvas");
 let ctx=canvas.getContext("2d");
+let canvas2=document.getElementById("canvas2");
+let ctx2=canvas2.getContext("2d");
 
-// retângulos
-ctx.beginPath();
-ctx.lineWidth = 0;
-ctx.fillStyle = 'red';
-ctx.strokeRect(0,0,50,50);
-ctx.fillRect(0,0,50,50);
-ctx.closePath();
+function quadrado_pequeno(x, y, cor){
+    ctx.beginPath();
+    ctx.fillStyle = cor;
+    ctx.fillRect(x,y,30,30);
+    ctx.closePath();
+}
 
-// retângulos
-ctx.beginPath();
-ctx.lineWidth = 0;
-ctx.fillStyle = 'yellow';
-ctx.strokeRect(0,350,50,400);
-ctx.fillRect(0,350,50,400);
-ctx.closePath();
+function quadrado_grande(x, y, cor){
+    ctx.beginPath();
+    ctx.fillStyle = cor;
+    ctx.fillRect(x,y,45,45);
+    ctx.closePath();
+}
 
-// retângulos
-ctx.beginPath();
-ctx.lineWidth = 0;
-ctx.fillStyle = 'blue';
-ctx.strokeRect(350,0,400,50);
-ctx.fillRect(350,0,400,50);
-ctx.closePath();
+function quadrado_medio(x, y, cor){
+    ctx.beginPath();
+    ctx.fillStyle = cor;
+    ctx.fillRect(x,y,35,35);
+    ctx.closePath();
+}
 
-// retângulos
-ctx.beginPath();
-ctx.lineWidth = 0;
-ctx.fillStyle = 'green';
-ctx.strokeRect(350,350,400,400);
-ctx.fillRect(350,350,400,400);
-ctx.closePath();
+function linha(x1, y1, x2, y2, cor){
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = cor;
+    ctx.moveTo(x1,y1);
+    ctx.lineTo(x2,y2);
+    ctx.stroke();
+    ctx.closePath();
+}
 
-// arcos
-ctx.beginPath();
-ctx.lineWidth = 2;
-ctx.strokeStyle = 'green';
-ctx.fillStyle = 'white';
-ctx.arc(200,200,50,0,Math.PI);
-ctx.stroke();
-ctx.fill();
-ctx.closePath();
+function arco(x1, y1, r, ang1, ang2, cor1 ,cor2){
+    ctx.beginPath();
+    ctx.lineWidth = 1.25;
+    ctx.strokeStyle = cor1;
+    ctx.fillStyle = cor2;
+    ctx.arc(x1,y1,r,ang1,ang2);
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
 
-// arcos
-ctx.beginPath();
-ctx.lineWidth = 2;
-ctx.strokeStyle = 'green';
-ctx.fillStyle = 'yellow';
-ctx.arc(330,125,20,0,2*Math.PI);
-ctx.stroke();
-ctx.fill();
-ctx.closePath();
+}
 
-// linhas
-ctx.beginPath();
-ctx.lineWidth = 2;
-ctx.strokeStyle = 'red';
-ctx.moveTo(0,0);
-ctx.lineTo(400,400);
-ctx.stroke();
-ctx.closePath();
+function texto(x, y, texto){
+    ctx.beginPath();
+    ctx.lineWidth = 10;
+    ctx.fillStyle = "black";
+    ctx.font = "20px Arial";
+    ctx.fillText(texto,x,y);
+    ctx.closePath();
 
-// linhas
-ctx.beginPath();
-ctx.lineWidth = 2;
-ctx.strokeStyle = 'blue';
-ctx.moveTo(0,400);
-ctx.lineTo(400,0);
-ctx.stroke();
-ctx.closePath();
+}
 
-// linhas
-ctx.beginPath();
-ctx.lineWidth = 2;
-ctx.strokeStyle = 'green';
-ctx.moveTo(0,200);
-ctx.lineTo(400,200);
-ctx.stroke();
-ctx.closePath();
+quadrado_grande(0, 0, "blue");
+quadrado_grande(255, 0, "red");
 
-// arcos
-ctx.beginPath();
-ctx.lineWidth = 2;
-ctx.strokeStyle = 'green';
-ctx.fillStyle = 'yellow';
-ctx.arc(70,125,20,0,2*Math.PI);
-ctx.stroke();
-ctx.fill();
-ctx.closePath();
+linha(40, 40, 150, 150, "blue");
+linha(260, 40, 150, 150, "red");
 
-// texto
-ctx.beginPath();
-ctx.lineWidth = 10;
-ctx.fillStyle = 'black';
-ctx.font = "25px Arial";
-ctx.fillText("Desenvolvimento Web",75,75);
-ctx.closePath();
+quadrado_pequeno(0, 120, "cyan");
+quadrado_pequeno(0, 150, "cyan");
+quadrado_pequeno(270, 135, "cyan");
+
+quadrado_medio(115, 150, "red")
+
+linha(0, 150, 300, 150, "green");
+linha(150, 260, 150, 150, "black")
+
+quadrado_pequeno(0, 240, "yellow");
+quadrado_pequeno(0, 270, "yellow");
+quadrado_pequeno(30, 270, "yellow");
+
+quadrado_pequeno(270, 240, "black");
+quadrado_pequeno(270, 270, "black");
+quadrado_pequeno(240, 270, "black");
+
+arco(150, 300, 40, Math.PI, 0, "green", "cyan")
+arco(150, 300, 80, Math.PI, 1.5*Math.PI, "green", "transparent")
+arco(150, 300, 60, 1.5*Math.PI, 0, "green", "transparent")
+
+arco(75, 225, 15, 0, 2.5*Math.PI, "green", "yellow")
+arco(225, 225, 15, 0, 2.5*Math.PI, "green", "yellow")
+
+arco(150, 120, 15, 0, 2*Math.PI, "blue", "cyan")
+
+arco(150, 150, 80, Math.PI, 1.25*Math.PI, "green", "transparent")
+arco(150, 150, 80, 1.75*Math.PI, 0, "green", "transparent")
+arco(150, 150, 60, Math.PI, 0, "green", "transparent")
+
+texto (110, 45, "Canvas")
+
+
+
+//Casa
+
+
+

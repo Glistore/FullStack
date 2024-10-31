79cbdbdc84f513c97d91e47d733a01d415558870
Henrique Simões <henrique_barros_simoes@hotmail.com>
2024-10-31 15:45:17 -0300

    08

--- Laboratório/08 - Servidor - Get, Post e Template/servidor.js
+++ Laboratório/08 - Servidor - Get, Post e Template/servidor.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 app.set('views', './views');
@@ -16,24 +16,38 @@ server.listen(80);
 console.log('Servidor rodando ...'.rainbow);
 
 app.get('/', function (requisicao, resposta){
-    resposta.redirect('home.html')
+    resposta.redirect('project.html')
 })
 
-app.get('/inicio', function (requisicao, resposta){
-    var nome = requisicao.query.info;
-    console.log(nome);
-})
+app.get('/cadastra', function(requisicao, resposta){
+    let nome = requisicao.query.cadastra_nome;
+    let senha = requisicao.query.cadastra_senha;
+    console.log('Nome: ' + nome + ' Email: ' + email + ' Senha: ' + senha);
 
-app.post('/inicio', function (requisicao, resposta){
-    var data = requisicao.body.data;
-    console.log(data);
-})
+    resposta.redirect('sucesso.html');
 
-app.get('/cadastro',function (requisicao, resposta){
-    var nome = requisicao.query.nome;
-    var sobrenome = requisicao.query.sobrenome;
-    var nascimento = requisicao.query.nascimento;
-    var civil = requisicao.query.civil;
+});
 
-    resposta.render('resposta_cadastro', {nome, sobrenome, nascimento, civil})
-})
+
+app.post('/cadastra', function(requisicao, resposta){
+    let nome = requisicao.body.cadastra_nome;
+    let senha = requisicao.body.cadastra_senha;
+    console.log('Nome: ' + nome + ' Senha: ' + senha);
+
+    resposta.redirect('sucesso.html');
+
+});
+
+app.post('/login', function(requisicao, resposta){
+    let nome = requisicao.body.login_login;
+    let senha = requisicao.body.login_senha;
+    console.log('Nome: ' + nome + ' Senha: ' + senha);
+
+    
+    if(nome == "123" && senha == "123"){
+        resposta.render('resposta.ejs', {resposta: "sucesso", mensagem: "Login efetuado com sucesso"});
+    }
+    else{
+        resposta.render('resposta.ejs', {resposta: "erro", mensagem: "Login ou senha incorretos"});
+    }
+});

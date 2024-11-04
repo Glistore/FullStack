b671f117e1ed9d7ba80409e087bd412b8252b475
Henrique Simões <henrique_barros_simoes@hotmail.com>
2024-11-04 14:32:43 -0300

    08

--- Laboratório/08 - Servidor - Get, Post e Template/servidor.js
+++ Laboratório/08 - Servidor - Get, Post e Template/servidor.js
@@ -1,23 +1,17 @@
 var http = require('http');
 var express = require('express');
 var colors = require('colors');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 var app = express();
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: true}))
-app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 app.set('views', './views');
 
-var server = http.createServer(app);
-server.listen(80);
-
-console.log('Servidor rodando ...'.rainbow);
-
 app.get('/', function (requisicao, resposta){
     resposta.redirect('project.html')
-})
+});
 
 app.get('/cadastra', function(requisicao, resposta){
     let nome = requisicao.query.cadastra_nome;
@@ -44,10 +38,16 @@ app.post('/login', function(requisicao, resposta){
     console.log('Nome: ' + nome + ' Senha: ' + senha);
 
     
-    if(nome == "123" && senha == "123"){
+    if(nome == "abc" && senha == "123"){
         resposta.render('resposta.ejs', {resposta: "sucesso", mensagem: "Login efetuado com sucesso"});
     }
     else{
         resposta.render('resposta.ejs', {resposta: "erro", mensagem: "Login ou senha incorretos"});
     }
 });
+
+
+var server = http.createServer(app);
+server.listen(80);
+
+console.log('Servidor ON ...'.rainbow);

be12907c730bde89498649b382173cc9fabc7a3a
Henrique Simões <henrique_barros_simoes@hotmail.com>
2024-11-04 20:59:32 -0300

    Aula - 11

--- Teoria/Aula - 11/servidor.js
+++ Teoria/Aula - 11/servidor.js
@@ -1,54 +1,82 @@
+require('colors');
+console.log('Servidor iniciado'.rainbow);
+
 var http = require('http');
+var mongodb = require("mongodb");
 var express = require('express');
-var colors = require('colors');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+
+const MongoClient = mongodb.MongoClient;
+
+const uri = "mongodb+srv://henrique:cyOs7rd3jVT2PdTy@cluster0.bs8hh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
 
 var app = express();
-app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
-app.set('view engine', 'ejs')
-app.set('views', './views');
-
-var mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
-const uri = `mongodb+srv://henrique:<cyOs7rd3jVT2PdTy>@cluster0.bs8hh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
-var server = http.createServer(app);
-server.listen(80);
 
-console.log('Servidor rodando ...'.rainbow);
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-app.post("/cadastrar_usuario", function(req, resp) {
 
-    // salva dados no banco
-    client.db("exemplo_bd").collection("usuarios").insertOne(
-      { db_nome: req.body.nome, db_login: req.body.login, db_senha: req.body.senha }, function (err) {
-      if (err) {
-        resp.render('resposta_usuario', {resposta: "Erro ao cadastrar usuário!"})
-      }else {
-        resp.render('resposta_usuario', {resposta: "Usuário cadastrado com sucesso!"})       
-      };
-    });
- 
- });
 
- app.post("/logar_usuario", function(req, resp) {
 
-    // busca um usuário no banco de dados
-    client.db("exemplo_bd").collection("usuarios").find(
-      {db_login: req.body.login, db_senha: req.body.senha }).toArray(function(err, items) {
-        console.log(items);
-        if (items.length == 0) {
-          resp.render('resposta_usuario', {resposta: "Usuário/senha não encontrado!"})
-        }else if (err) {
-          resp.render('resposta_usuario', {resposta: "Erro ao logar usuário!"})
+app.get('/cadastra', function(req, res){
+    let nome = req.query.cadastra_nome;
+    let senha = req.query.cadastra_senha;
+    let login = req.query.cadastra_login
+    let nasc = req.query.cadastra_nascimento
+
+    client.db("Cluster0").collection("usuarios").insertOne(
+        { db_nome: nome, db_login: login, db_senha: senha, db_nascimento: nasc }, function (err) {
+        if (err) {
+          res.render('resposta.ejs', {resposta: "erro", mensagem: "Erro ao cadastrar"});
         }else {
-          resp.render('resposta_usuario', {resposta: "Usuário logado com sucesso!"})       
+          res.render('resposta.ejs', {resposta: "sucesso", mensagem: "Cadastro foi"});
+              
         };
       });
+   
+});
+
+
+
+
+app.post('/login', function(req, res){
+    let nome = req.body.login_login;
+    let senha = req.body.login_senha;
+    client.db("Cluster0").collection("usuarios").find(
+        {db_login: nome, db_senha: senha }).toArray(function(err, items) {
+          console.log(items);
+          if (items.length == 0) {
+            res.render('resposta.ejs', {resposta: "erro", mensagem: "Usuario não encontrado"});
+          }else if (err) {
+            res.render('resposta.ejs', {resposta: "erro", mensagem: "Erro ao logar"});
+          }else {
+            res.render('resposta.ejs', {resposta: "sucesso", mensagem: "Usuario Logado"});
+          };
+        });
+   
+});
+
+app.get('/imprime', function(req, res){
+   let qtd = req.query.qtd;
+
+   res.render('imprime.ejs', {qtd: qtd});
+
+});
+
+
  
- });
- 
- 
+
+
+
+var server = http.createServer(app);
+
+server.listen(80);
+
+
+console.log('Servidor rodando'.rainbow);
